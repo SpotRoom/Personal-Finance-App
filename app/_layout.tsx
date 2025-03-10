df22ec8667c1ce4efcb0c27b5373a88e8aadcc4a
nachatra-sharma <nakshusharm0@gmail.com>
2025-03-10 06:55:46 +0530

    added tailwind

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { SafeAreaView, Text } from "react-native";
+import "@/global.css";
 
 const Home = () => {
   return (
     <SafeAreaView>
-      <Text>Personal Finance Tracker App</Text>
+      <Text className="text-2xl">Hello world</Text>
+      <Text className="mx-10 text-2xl text-purple-600">
+        Personal Finance Tracker App
+      </Text>
     </SafeAreaView>
   );
 };
